97b4f47997ff137173cec6e4de1af14b5b0cb43c
GitMoons <GitMoons@github.com>
2018-01-03 19:32:52 +0800

    SpringBoot框架初始化
    基础框架

---
+++ src/main/java/com/jlu/blogwall/BlogwallApplication.java
@@ -0,0 +1,12 @@
+package com.jlu.blogwall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogwallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogwallApplication.class, args);
+	}
+}
